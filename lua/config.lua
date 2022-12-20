c25f70e161fd1b8aa6ea74999030e9f12e7eee6a
Rickard Andersson <gonz@severnatazvezda.com>
2022-12-20 22:40:35 +0200

    add terminal binding group for <leader>t
    Plus a binding for ESC to exit terminal insert mode

--- lua/config.lua
+++ lua/config.lua
@@ -13,10 +13,13 @@ require("completion")
 require("debugging")
 require("statusline")
 
+-- Sourcing configuration files
 vim.keymap.set("n", "<leader><leader>S", "<cmd>source ~/.config/nvim/lua/snippets.lua<CR>")
 vim.keymap.set("n", "<leader><leader>L", "<cmd>source ~/.config/nvim/lua/config.lua<CR>")
 vim.keymap.set("n", "<leader><leader>R", "<cmd>source ~/.config/nvim/init.vim<CR>")
 vim.keymap.set("n", "<leader><TAB>", "<C-6>")
+-- Exiting insert mode in the terminal
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
 
 require("which")
 require("comments")
--- lua/which.lua
+++ lua/which.lua
@@ -1,6 +1,12 @@
 local whichKey = require("which-key")
 
 whichKey.register(
-  {},
+  {
+    t = {
+      name = "Terminal",
+      -- Opens a terminal on the right side of the screen
+      t = { "<cmd>rightbelow vnew +terminal<CR>", "Terminal" },
+    },
+  },
   { prefix = "<leader>" }
 )
