bbcb8276442e5a1bb42184df2f2edb76c2b1422f
Rickard Andersson <gonz@severnatazvezda.com>
2022-12-24 10:51:36 +0200

    add binding for toggling nerdtree

--- lua/which.lua
+++ lua/which.lua
@@ -2,23 +2,26 @@ local whichKey = require("which-key")
 
 whichKey.register(
   {
-    t = {
-      name = "Terminal",
-      -- Opens a terminal on the right side of the screen
-      t = { "<cmd>rightbelow vnew +terminal<CR>", "Terminal" },
-      -- Opens a new terminal in a new tab
-      T = { "<cmd>tabnew +terminal<CR>", "Terminal Tab" },
+    ["<leader>"] = {
+      t = {
+        name = "Terminal",
+        -- Opens a terminal on the right side of the screen
+        t = { "<cmd>rightbelow vnew +terminal<CR>", "Terminal" },
+        -- Opens a new terminal in a new tab
+        T = { "<cmd>tabnew +terminal<CR>", "Terminal Tab" },
+      },
+      G = {
+        name = "git",
+        P = { "<cmd>Git push<CR>", "Push" },
+        G = { "<cmd>Git<CR>", "Status" },
+        p = { "<cmd>Git pull<CR>", "Pull" },
+        C = { "<cmd>Git commit<CR>", "Commit" },
+        B = { "<cmd>Git blame<CR>", "Blame" },
+        L = { "<cmd>Git log<CR>", "Log" },
+        l = { "<cmd>Telescope git_commits<CR>", "Log" },
+      },
     },
-    G = {
-      name = "git",
-      P = { "<cmd>Git push<CR>", "Push" },
-      G = { "<cmd>Git<CR>", "Status" },
-      p = { "<cmd>Git pull<CR>", "Pull" },
-      C = { "<cmd>Git commit<CR>", "Commit" },
-      B = { "<cmd>Git blame<CR>", "Blame" },
-      L = { "<cmd>Git log<CR>", "Log" },
-      l = { "<cmd>Telescope git_commits<CR>", "Log" },
-    },
+    ["-"] = { "<cmd>NERDTreeToggle<CR>", "Toggle NERDTree" },
   },
-  { prefix = "<leader>" }
+  {}
 )
