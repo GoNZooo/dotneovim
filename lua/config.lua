bbd29674b4306b5d7657196db4ad43cd97e4f206
Rickard Andersson <gonz@severnatazvezda.com>
2023-04-26 14:44:24 +0300

    remove sourcing binds that were moved to `which`

--- lua/config.lua
+++ lua/config.lua
@@ -13,11 +13,6 @@ require("completion")
 require("debugging")
 require("statusline")
 
--- Sourcing configuration files
-vim.keymap.set("n", "<leader><leader>S", "<cmd>source ~/.config/nvim/lua/snippets.lua<CR>")
-vim.keymap.set("n", "<leader><leader>L", "<cmd>source ~/.config/nvim/lua/config.lua<CR>")
-vim.keymap.set("n", "<leader><leader>R", "<cmd>source ~/.config/nvim/init.vim<CR>")
-vim.keymap.set("n", "<leader><TAB>", "<C-6>")
 -- Exiting insert mode in the terminal
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
 
