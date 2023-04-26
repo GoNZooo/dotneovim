4d070bc2357d983211b26e0659bd793b24c175d6
Rickard Andersson <gonz@severnatazvezda.com>
2023-04-26 14:43:47 +0300

    add new key for showing error float

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -4,6 +4,7 @@ local lspconfig = require("lspconfig")
 
 local opts = { noremap = true, silent = true }
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, opts)
+vim.keymap.set("n", "<C-e>", vim.diagnostic.open_float, opts)
 vim.keymap.set("n", "<leader>ep", vim.diagnostic.goto_prev, opts)
 vim.keymap.set("n", "<leader>en", vim.diagnostic.goto_next, opts)
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, opts)
