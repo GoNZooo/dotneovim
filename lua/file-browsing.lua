2eca496218a5ac6f25a00dbb0d3fac20432af6e9
Rickard Andersson <gonz@severnatazvezda.com>
2023-09-11 21:00:43 +0300

    set keymap binds explicitly for `oil`

--- lua/file-browsing.lua
+++ lua/file-browsing.lua
@@ -1,1 +1,26 @@
-require("oil").setup()
+require("oil").setup({
+  columns = {
+      "icon",
+      "permissions",
+  },
+  keymaps = {
+    ["g?"] = "actions.show_help",
+    ["<CR>"] = "actions.select",
+    ["gv"] = "actions.select_vsplit",
+    ["gh"] = "actions.select_split",
+    ["gt"] = "actions.select_tab",
+    ["gp"] = "actions.preview",
+    ["<C-c>"] = "actions.close",
+    ["-"] = "actions.parent",
+    ["_"] = "actions.open_cwd",
+    ["`"] = "actions.cd",
+    ["~"] = "actions.tcd",
+    ["gs"] = "actions.change_sort",
+    ["g."] = "actions.toggle_hidden",
+    ["gr"] = "actions.refresh",
+  },
+  use_default_keymaps = false,
+  view_options = {
+      show_hidden = true,
+  },
+})
